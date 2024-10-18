aef05eb34e181fc4e798158fedb0f7ee9fda7101
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-17 21:55:39 -0600

    feat: add Main class to demonstrate PasswordGenerator; docs: include usage comments

--- src/main/java/com/gklyphon/Main.java
+++ src/main/java/com/gklyphon/Main.java
@@ -1,7 +1,25 @@
 package com.gklyphon;
 
+import com.gklyphon.generator.PasswordGenerator;
+
+/**
+ * Main class to demonstrate the usage of the PasswordGenerator.
+ * Generates and prints a password to the console.
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 17-Oct-2024
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            PasswordGenerator passwordGenerator = new PasswordGenerator();
+            String password = passwordGenerator.generate();
+            System.out.println("Generated Password: " + password);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("An unexpected error ocurred: " + e.getMessage());
+        }
     }
 }
